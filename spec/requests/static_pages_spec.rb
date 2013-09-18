be35513bd1471d35006d97f81f16ffa67f07cc68
Tom Powell <tompowell@gmail.com>
2013-09-17 22:52:44 -0400

    added sign up page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,14 +1,11 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject {page}
   describe "Home page" do
-    it "should have the content 'Sick of not knowing'" do 
-      visit '/static_pages/home' 
-      expect(page).to have_content('Sick of not knowing')
-    end
-    it "should have the right title" do
-  		visit '/static_pages/home'
-  		expect(page).to have_title("Keep Tabs on your team with TeamStatus App")
-		end
+      before { visit root_path }
+      it { should have_content('Sick of not knowing') }
+      it { should have_title("Keep Tabs on your team with TeamStatus App") }
+      # it { should have_  expect(button).to have 
   end
 end
