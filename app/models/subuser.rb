5c53d9725e91596980f35d7a751cca44822e7280
Tom Powell <tompowell@gmail.com>
2013-10-26 18:00:05 -0400

    no idea whats going on

---
+++ app/models/subuser.rb
@@ -0,0 +1,18 @@
+class Subuser < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :invitable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :invitable
+
+	before_save { self.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+	
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+										uniqueness: { case_sensitive: false }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131005204702) do
+ActiveRecord::Schema.define(version: 20131025024010) do
+
+  create_table "subusers", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "subusers", ["email"], name: "index_subusers_on_email", unique: true
+  add_index "subusers", ["reset_password_token"], name: "index_subusers_on_reset_password_token", unique: true
 
   create_table "users", force: true do |t|
     t.string   "name"
